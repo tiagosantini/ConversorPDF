4cb921111ad8e661dd18b231b01957c803e03c80
lucasdetoffollemos <lucasdetoffollemos@gmail.com>
2021-10-25 15:31:35 -0300

    Classe ArquivoDeSaida criada

---
+++ ConversorPDF.WindowsService/Dominio/ArquivoDeSaida.cs
@@ -0,0 +1,57 @@
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorPDF.WindowsService.Dominio
+{
+    public class ArquivoDeSaida
+    {
+        private readonly string arquivo;
+        private readonly string diretorio;
+        private readonly string conteudo;
+
+
+        public ArquivoDeSaida(string arquivo, string diretorio, string conteudo)
+        {
+            this.arquivo = arquivo;
+            this.diretorio = diretorio;
+            this.conteudo = conteudo;
+        }
+
+        public bool GerarArquivoPdf(out string nomeArquivo)
+        {
+            nomeArquivo = Path.GetFileNameWithoutExtension(arquivo);
+
+            try
+            {
+                var nome = string.Format(@"{0}\{1}.pdf", diretorio, nomeArquivo);
+
+                using var fileStream = new FileStream(nome, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+
+                using PdfWriter writer = new PdfWriter(fileStream);
+
+                using PdfDocument pdf = new PdfDocument(writer);
+
+                using Document document = new Document(pdf);
+
+                Paragraph texto = new Paragraph(conteudo)
+                  .SetTextAlignment(TextAlignment.LEFT);
+
+                document.Add(texto);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
